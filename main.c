99c5da16e9cd198fdf08069fc43fca8b4a8fec0b
aspcodenet <stefan.holmberg@systementor.se>
2023-11-22 10:20:48 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+
+// TA PINNEN nr 5
+
+int main(){
+    while(1){
+        // SÄTTA pinnen
+        //sleep (1000)
+        // CLEARA pinnen
+        //sleep (1000)
+    }
+    return 0;
+}
+
