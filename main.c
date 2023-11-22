ff6c935c484bd40955ce5128691b559c31da5c19
aspcodenet <stefan.holmberg@systementor.se>
2023-11-22 10:45:43 +0100

    aaaa

--- main.c
+++ main.c
@@ -1,17 +1,34 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
-// TA PINNEN nr 5
+// ARDUINO NR 10
+// PORTB + pinne 2
+
+
+// ARDUINO UNO 0
+// PORTD + pinne 0
+
+// https://wokwi.com/projects/365047092561218561
 // B (digital pin 8 to 13)
 // C (analog input pins)
 // D (digital pins 0 to 7)
 
+#define BIT_SET(a, b) ((a) |= (1ULL << (b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+
 
 int main(){
+    //init
+
     while(1){
-        // SÄTTA pinnen
-        //sleep (1000)
+       // SÄTTA pinnen
+       BIT_SET(PORTB,2);
+       _delay_ms(1000);
         // CLEARA pinnen
-        //sleep (1000)
+       BIT_CLEAR(PORTB,2);
+       _delay_ms(1000);
     }
     return 0;
 }
